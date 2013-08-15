9192df17ad6b5e758fd8e311b597398c8df44f3b
Anders Mårtén <anders.marten@ungap.com>
2013-08-15 16:34:39 +0200

    Added Javascript for calling the REST service

---
+++ App_Code/Iwcf.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace RESTFulNotes
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "Iwcf" in both code and config file together.
+    [ServiceContract]
+    public interface Iwcf
+    {
+        [OperationContract]
+        void DoWork();
+
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json, UriTemplate = "Notes?tag={tag}")]
+        [OperationContract]
+        Notes FindNotes(string tag);
+    }
+
+    [DataContract]
+    public class Note
+    {
+        [DataMember]
+        public int ID { get; set; }
+        [DataMember]
+        public string Category { get; set; }
+        [DataMember]
+        public string Subject { get; set; }
+        [DataMember]
+        public string NoteText { get; set; }
+    }
+
+    [CollectionDataContract]
+    public class Notes : List<Note>
+    {
+        public Notes() { }
+        public Notes(List<Note> Notes) : base(Notes) { }
+    }
+}
---
+++ App_Code/wcf.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace RESTFulNotes
+{
+
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "wcf" in code, svc and config file together.
+    public class wcf : Iwcf
+    {
+        public void DoWork()
+        {
+        }
+
+        public Notes FindNotes(string tag)
+        {
+            WebOperationContext.Current.OutgoingResponse.StatusCode = HttpStatusCode.OK;
+            Notes notes = new Notes();
+
+            Note note = new Note();
+
+            note.ID = 1965;
+            note.Category = "Man";
+            note.Subject = "Programming with WCF";
+            note.NoteText = "Hope he can create a generic REST service";
+            notes.Add(note);
+
+            return notes;
+        }
+    }
+}
